e5ea676eb529d98b3ab3418cc631d85b7d15aece
JJ Asghar <jj@chef.io>
2018-06-01 12:05:07 -0500

    updates

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://www.rubygems.org'
+
+gem 'chef'
+gem 'chef-provisioning-vsphere', :path => '../chef-provisioning-vsphere'
+gem 'pry'
---
+++ cookbooks/provision/recipes/default.rb
@@ -0,0 +1,34 @@
+require 'chef/provisioning'
+require 'chef/provisioning/vsphere_driver'
+
+#
+# This is the main way to connect to vSphere
+#
+with_vsphere_driver host: '172.16.20.2',
+  insecure: true,
+  user:     'administrator@vsphere.local',
+  password: 'Good4bye!'
+
+#
+# These are the machine_options that you need to declare
+#
+with_machine_options :bootstrap_options => {
+                               num_cpus: 2,
+                               memory_mb: 4096,
+                               datacenter: 'Datacenter',
+                               resource_pool: 'Cluster',
+                               template_name: 'centos7-tmpl',
+                               :ssh => {
+                                 :user => 'root',
+                                 :password => 'admini',
+                                 :paranoid => false,
+                               }
+                             },
+                :sudo => true
+
+#
+# This is where you can declare the machine
+#
+machine "testing-centos" do
+  tag "haha"
+end
